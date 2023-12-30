104c233027d8164d205eb163724083c75a32eb59
shubhanker saxena <ankursaxena347@gmail.com>
2023-12-31 02:30:05 +0530

    wip

---
+++ app/AboutUs/WhatWeDoSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function WhatWeDoSection() {
+  return (
+    <div>
+        <div className="flex flex-col text-center gap-y-5">
+            <div className="text-2xl font-bold">
+               WHAT WE DO?
+            </div>
+            <div className="text-lG font-normal leading-tight">
+               We are providing following best services.
+            </div>
+        </div>
+      <div className="grid grid-cols-2 gap-4">
+           
+      </div>
+    </div>
+  )
+}
+
+export default WhatWeDoSection;
--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import HeroSection from './HeroSection'
 import Container from '../container'
 import AboutSection from './AboutSection'
+import WhatWeDoSection from './WhatWeDoSection'
 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
          <HeroSection />
        
             <AboutSection />
+            <WhatWeDoSection />
          
     
      </>
