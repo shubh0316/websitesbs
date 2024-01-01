4748b851952ecccd9a0361c49899e744934ec5c4
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-02 02:44:07 +0530

    pricing section-done

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -3,19 +3,16 @@ import HeroSection from './HeroSection'
 import Container from '../container'
 import AboutSection from './AboutSection'
 import WhatWeDoSection from './WhatWeDoSection'
+import WebsitePackagesSection from './WebsitePackagesSection'
 
 
 export default function Home() {
   return (
-
-     
      <>
          <HeroSection />
-       
-            <AboutSection />
-            <WhatWeDoSection />
-         
-    
+         <AboutSection />
+         <WhatWeDoSection />
+         <WebsitePackagesSection />
      </>
   )
 }
