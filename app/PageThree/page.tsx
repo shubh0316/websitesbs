e9c3fca81a46a537dd13d56a58122a4abc6e463f
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-04 04:16:57 +0530

    wip

--- app/PageThree/page.tsx
+++ app/PageThree/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SectionOne from './SectionOne'
 import SectionTwo from './SectionTwo'
+import PricingPlan from './PricingPlan'
 
 function page() {
   return (
     <div>
       <SectionOne />
       <SectionTwo />
+      <PricingPlan />
     </div>
   )
 }
