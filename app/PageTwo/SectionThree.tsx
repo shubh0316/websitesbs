ab47b3bbda65852724eed47d0b0f06902254143b
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-08 02:30:01 +0530

    one-more-page-down-second

--- app/PageTwo/SectionThree.tsx
+++ app/PageTwo/SectionThree.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
-import image1 from "@/assets/PageTwo1.png";
+import image1 from "@/assets/BlockChain.png";
 
 function SectionThree() {
   return (
     <div>
-        <div className="grid grid-cols-2 md:gap-8 gap-6 p-10 ">
+        <div className="grid grid-cols-2 md:gap-8 gap-6 p-10 md:mt-4">
         <div className="md:col-span-1 col-span-2 md:order-first order-last">
                     <Image src={image1} alt="" className='w-[95%] h-[80%]' /> 
                 </div>
                <div className="grid md:col-span-1 col-span-2">
               
-                <div className="flex flex-col gap-4">
-                    <div className="text-extrabold">SOFTWARE DEVELOPMENT</div>
-                    <span className='text-md text-slate-700 font-normal'>Web Application, Android Apps, IOS Apps, Windows Apps</span>
+                <div className="flex flex-col gap-4 font-sans">
+                    <div className="font-bold">BLOCKCHAIN DEVELOPMENT</div>
+                    <span className='text-md text-slate-700 font-normal'>Cryptocurrency (Digital Currency), smart contracts, peer to peer, Proof of Work and cryptography technology</span>
                     <span className="text-md text-slate-700 font-normal">
-                    MLM Consultant Asia is a leading India based Software Company focused on delivering the best and most cost-effective solutions to our clients. Mini InfoTech provide variety of software services, Software Development Services, Web Development Services, Web Designing Services, MLM Software and website, Hosting services and much more. We have served our clients with Comprehensive Web Solutions - right From Strategic Planning and Online Marketing Decisions to the actual Designing, Development and Implementation with testing of the Projects, and our Support Team is always there for the Maintenance and Enhancement of the Project.
+                    The Blockchain consists of individual behavior specifications, a large set of rules that are programmed into it. Those specifications are called protocols. The implementation of specific protocols essentially made Blockchain what it is — a distributed, peer-to-peer and secured information database.
+                    </span>
+                    <span className="text-md text-slate-700 font-normal">
+                    The database is the Blockchain and each node on a Blockchain has access to the whole Blockchain. No one node or computer regulates the information it contains. Every node is able to validate the records of the Blockchain. This is all done without one or several intermediaries in control of everything.
                     </span>
                 </div>
                </div>
