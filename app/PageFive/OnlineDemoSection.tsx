c444a50cd9c6a2e282b5c01654dde0e0c0526cee
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-08 16:47:00 +0530

    demo

--- app/PageFive/OnlineDemoSection.tsx
+++ app/PageFive/OnlineDemoSection.tsx
@@ -6,62 +6,61 @@ const OnlineDemoSection: React.FC = () => {
     <div className="bg-orange-500 p-8">
       <div className="max-w-6xl mx-auto bg-white p-10 rounded-2xl shadow-lg">
       <div className="flex flex-col text-center gap-y-5 py-10">
-        <div className="text-2xl font-bold">EXTRA FEATURES</div>
-        <div className="text-sm font-normal leading-tight">
-          We are providing the following best services.
+        <div className="text-2xl font-bold">ONLINE FREE DEMO</div>
+        <div className="text-sm font-normal leading-tight text-slate-600">
+        Fill The Form & Get Free Online Demo Login Details
         </div>
       </div>
-        <div className="grid md:grid-cols-4 gap-10">
-            
+        <div className="grid md:grid-cols-4 gap-10">            
           <div className="col-span-1 text-start">
-            <h3 className="text-md mb-2">Heading 1</h3>
+            <h3 className="text-md mb-2 font-bold">Name</h3>
             <div className="rounded-full p-4 shadow-md">
               <input
                 type="text"
                 className="w-full h-full outline-none"
-                placeholder="Placeholder 1"
+                placeholder="Name"
               />
             </div>
           </div>
 
-          {/* Input 2 */}
+        
           <div className="col-span-1 text-start">
-            <h3 className="text-md mb-2">Heading 2</h3>
+            <h3 className="text-md mb-2 font-bold">Email</h3>
             <div className="rounded-full p-4 shadow-md">
               <input
                 type="text"
                 className="w-full h-full outline-none"
-                placeholder="Placeholder 2"
+                placeholder="Email"
               />
             </div>
           </div>
 
           {/* Input 3 */}
           <div className="col-span-1 text-start">
-            <h3 className="text-md mb-2">Heading 3</h3>
+            <h3 className="text-md mb-2 font-bold">Phone Number</h3>
             <div className="rounded-full p-4 shadow-md">
               <input
                 type="text"
                 className="w-full h-full"
-                placeholder="Placeholder 3"
+                placeholder="Phone Number"
               />
             </div>
           </div>
 
           {/* Input 4 */}
           <div className="col-span-1 text-start">
-            <h3 className="text-md mb-2">Heading 4</h3>
+            <h3 className="text-md mb-2 font-bold">Demo Request For</h3>
             <div className="rounded-full p-4 shadow-md">
               <input
                 type="text"
                 className="w-full h-full outline-none"
-                placeholder="Placeholder 4"
+                placeholder="service"
               />
             </div>
           </div>
         </div>
 
-        {/* Headings and Submit Button */}
+      
         <div className="mt-8 flex items-center md:justify-end">  
           <button className="bg-blue-500 text-white px-20 py-2 rounded-full">
             Submit
