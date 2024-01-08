332364588495211507d24524e0cbab80146e9b5e
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-08 13:05:01 +0530

    one-more-page-down

--- app/PageThree/SectionThree.tsx
+++ app/PageThree/SectionThree.tsx
@@ -2,46 +2,46 @@ import React from "react";
 
 const SectionThree = () => {
   return (
-    <div>
-      <div className="bg-slate-100">
-        <div className="flex flex-col text-center gap-y-5 py-10 font-sans">
-          <div className="text-2xl font-bold">OUR PACKAGES</div>
-          <div className="text-lG font-normal leading-tight">
-            We are providing following best services.
+    <div className="bg-slate-100 flex flex-col items-center justify-center py-10 h-full">
+      <div className="text-2xl font-bold mb-3">OUR FEATURES</div>
+      <div className="text-md text-slate-600 font-normal leading-tight mb-8">
+        We are providing the following best services.
+      </div>
+      
+      <div className="flex flex-col md:flex-row gap-6 w-full max-w-screen-xl justify-center">
+       
+        <div className="flex flex-col items-center transition duration-500 ease-in-out hover:scale-110 p-4 max-w-md">
+          <div className="flex flex-col space-y-6 bg-white rounded-2xl p-8">
+            <div className="text-2xl font-semibold text-start">Small Business</div>
+            <div className="text-md font-normal text-start">Basic e-commerce Package</div>
+            <div className="text-2xl font-normal text-start">$999.999</div>
+            <div className="w-full border border-blue-500 bg-white mt-8 text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
+              View Package
+            </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row items-center p-10 md:ml-20 gap-8 font-sans">
-  <div className="transition duration-500 ease-in-out hover:scale-110 p-4">
-    <div className="flex flex-col space-y-6 bg-white rounded-2xl p-8">
-      <div className="text-2xl font-semibold text-start">Small Business</div>
-      <div className="text-md font-normal text-start">Basic e-commerce Package</div>
-      <div className="text-2xl font-normal text-start">$999.999</div>
-      <div className="w-full md:w-80 border border-blue-500 bg-white mt-8 md:mt-12 text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
-        View Package
-      </div>
-    </div>
-  </div>
-          <div className=" transition duration-500 ease-in-out items-center hover:scale-110 p-2">
-            <span className="bg-orange-500 text-white p-2 text-center px-20 md:px-[8rem] rounded-t-2xl">Most Popular</span>
-  <div className="flex flex-col space-y-6 bg-white  rounded-2xl p-4">
-    <div className="text-2xl font-semibold text-start">Mid-Size Business</div>
-    <div className="text-md font-normal text-start">Pro e-Commerce Package</div>
-    <div className="text-2xl font-normal text-start">$1999.999</div>
-    <div className="flex items-start justify-start">
-  <div className="w-full md:w-80 border border-blue-500 bg-white mt-4 text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
-    View Package
-  </div>
-    </div>
-  </div>
-</div>
-          <div className=" transition duration-500 ease-in-out hover:scale-110 p-4">
-            <div className="flex flex-col space-y-6 bg-white rounded-2xl p-8">
-              <div className="text-2xl font-semibold text-start">Large Business</div>
-              <div className="text-md font-normal text-start">B2B e-Commerce Package</div>
-              <div className="text-2xl font-normal text-start">$3999.999</div>
-              <div className="w-full md:w-80 border border-blue-500 bg-white mt-8 md:mt-12 py-2 text-center text-blue-500  transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
-                View Package
-              </div>
+
+        <div className="flex flex-col items-center transition duration-500 ease-in-out hover:scale-110 p-4 max-w-md">
+          <span className="bg-orange-500 text-white p-2 text-center w-full rounded-t-2xl">
+            Most Popular
+          </span>
+          <div className="flex flex-col space-y-6 bg-white rounded-2xl p-4 w-full">
+            <div className="text-2xl font-semibold text-start">Mid-size Business</div>
+            <div className="text-md font-normal text-start">Pro e-Commerce Package</div>
+            <div className="text-2xl font-normal text-start">$1999.999</div>
+            <div className="w-full border border-blue-500 bg-white mt-4 text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
+              View Package
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center transition duration-500 ease-in-out hover:scale-110 p-4 max-w-md">
+          <div className="flex flex-col space-y-6 bg-white rounded-2xl p-8">
+            <div className="text-2xl font-semibold text-start">Large Business</div>
+            <div className="text-md font-normal text-start">B2B e-commerce Package</div>
+            <div className="text-2xl font-normal text-start">$3999.999</div>
+            <div className="w-full border border-blue-500 bg-white mt-8 text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
+              View Package
             </div>
           </div>
         </div>
--- app/PageThree/SectionTwo.tsx
+++ app/PageThree/SectionTwo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SectionTwo() {
   return (
     <div>
-        <div className='bg-slate-100'>
+        <div className='bg-white'>
         <div className="flex flex-col text-center gap-y-5 py-10">
             <div className="text-2xl font-bold">
                OUR FEATURES
@@ -13,7 +13,7 @@ function SectionTwo() {
             </div>
         </div>
     
-    <div className="grid  grid-rows-3 md:items-center  p-10 space-y-10 font-sans">
+    <div className="grid  grid-rows-3 md:items-center  p-10 sm:space-y-4 ">
         <div className='p-8  md:row-span-1 bg-white rounded-lg'>  
        <div className="font-bold text-orange-600 text-2xl">
               Fully Dynamic Package
@@ -299,13 +299,13 @@ function SectionTwo() {
      
        </div>
        </div>
-       <div className='p-8  md:row-span-1 bg-white rounded-lg'>  
+       <div className='p-8  md:row-span-1 bg-white rounded-lg '>  
        <div className="font-bold text-orange-600 text-2xl">
               Backend User Feature 
        </div>
        <div className='grid md:grid-cols-3 gap-4 md:gap-20 p-4'>
         <div className="grid col-span-1 ">
-            <div className='flex flex-col space-y-4 '>
+            <div className='flex flex-col space-y-4 font-sans'>
             <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
               <span className="text-md">
                 <svg
@@ -398,7 +398,7 @@ function SectionTwo() {
 
         </div>
         <div className="grid col-span-1">
-            <div className='flex flex-col space-y-4 '>
+            <div className='flex flex-col space-y-4 font-sans'>
             <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
               <span className="text-md">
                 <svg
@@ -586,14 +586,14 @@ function SectionTwo() {
 
        </div>
        </div>
-       <div className='p-8  md:row-span-1 bg-white rounded-lg'>  
-       <div className="text-extrabold text-orange-700 text-lg">
-              Fully Dynamic Package
+       <div className='p-8  md:row-span-1 bg-white rounded-lg font-sans'>  
+       <div className="font-bold text-orange-600 text-2xl">
+              Backend Admin Feature
        </div>
        <div className='grid md:grid-cols-3 gap-4 md:gap-20 p-4'>
         <div className="grid col-span-1 ">
-            <div className='flex flex-col space-y-4  md:space-y-8 justify-content space-between'>
-            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
+            <div className='flex flex-col space-y-4   justify-content space-between'>
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4 font-sans">
               <span className="text-md">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -620,7 +620,8 @@ function SectionTwo() {
                   </defs>
                 </svg>
               </span>
-              <span></span>
+              <span className='text-sm w-2/3'>Easy to use Admin – Not much technical knowledge required</span>
+
             </div>
             <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
               <span className="text-md">
@@ -649,7 +650,8 @@ function SectionTwo() {
                   </defs>
                 </svg>
               </span>
-              <span></span>
+              <span className='text-sm w-2/3'>Add/ modify / delete products and related information like Price, Image, Description, title, model number, stock, etc.</span>
+
             </div>
             <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
               <span className="text-md">
@@ -678,14 +680,170 @@ function SectionTwo() {
                   </defs>
                 </svg>
               </span>
-              <span></span>
+              <span className='text-sm w-2/3'>Ability to manage the content of static pages like about us, terms and condition, etc.</span>
+
+            </div>
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4 font-sans">
+              <span className="text-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_3998_738)">
+                    <path
+                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
+                      fill="#565656"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_3998_738">
+                      <rect
+                        width="19.9999"
+                        height="19.9997"
+                        fill="white"
+                        transform="translate(0 0.000244141)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+              <span className='text-sm w-2/3'>Invoice generation and printing.</span>
+
+            </div>
+            </div>
+
+        </div>
+        <div className="grid col-span-1">
+            <div className='flex flex-col space-y-8 font-sans'>
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
+              <span className="text-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_3998_738)">
+                    <path
+                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
+                      fill="#565656"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_3998_738">
+                      <rect
+                        width="19.9999"
+                        height="19.9997"
+                        fill="white"
+                        transform="translate(0 0.000244141)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+              <span className='text-sm w-2/3'>Password Protected admin Interface</span>
+
+            </div>
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
+              <span className="text-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_3998_738)">
+                    <path
+                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
+                      fill="#565656"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_3998_738">
+                      <rect
+                        width="19.9999"
+                        height="19.9997"
+                        fill="white"
+                        transform="translate(0 0.000244141)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+              <span className='text-sm w-2/3'>Ability to place a single product in multiple categories</span>
+
+            </div>
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
+              <span className="text-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_3998_738)">
+                    <path
+                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
+                      fill="#565656"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_3998_738">
+                      <rect
+                        width="19.9999"
+                        height="19.9997"
+                        fill="white"
+                        transform="translate(0 0.000244141)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+              <span className='text-sm w-2/3'>Inventory Stock Management on Product Level</span>
+
+            </div>
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
+              <span className="text-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_3998_738)">
+                    <path
+                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
+                      fill="#565656"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_3998_738">
+                      <rect
+                        width="19.9999"
+                        height="19.9997"
+                        fill="white"
+                        transform="translate(0 0.000244141)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+              <span className='text-sm w-2/3'>Generation of Various reports like, Most Viewed Products, Top orders by product, top sale by customers, etc.</span>
+
             </div>
             </div>
 
         </div>
         <div className="grid col-span-1">
             <div className='flex flex-col space-y-8'>
-            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
+            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4 font-sans">
               <span className="text-md">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -712,99 +870,8 @@ function SectionTwo() {
                   </defs>
                 </svg>
               </span>
-              <span></span>
-            </div>
-            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
-              <span className="text-md">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="15"
-                  height="15"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                >
-                  <g clip-path="url(#clip0_3998_738)">
-                    <path
-                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
-                      fill="#565656"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_3998_738">
-                      <rect
-                        width="19.9999"
-                        height="19.9997"
-                        fill="white"
-                        transform="translate(0 0.000244141)"
-                      />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </span>
-              <span></span>
-            </div>
-            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
-              <span className="text-md">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="15"
-                  height="15"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                >
-                  <g clip-path="url(#clip0_3998_738)">
-                    <path
-                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
-                      fill="#565656"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_3998_738">
-                      <rect
-                        width="19.9999"
-                        height="19.9997"
-                        fill="white"
-                        transform="translate(0 0.000244141)"
-                      />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </span>
-              <span></span>
-            </div>
-            </div>
+              <span className='text-sm w-2/3'>Ability to Add / modify / delete Categories, subcategories and related information</span>
 
-        </div>
-        <div className="grid col-span-1">
-            <div className='flex flex-col space-y-8'>
-            <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
-              <span className="text-md">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="15"
-                  height="15"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                >
-                  <g clip-path="url(#clip0_3998_738)">
-                    <path
-                      d="M16.9838 1.91626L6.80418 12.0997L2.9995 8.29508L-0.000488281 11.299L6.80027 18.0996L7.67918 17.2247L19.9877 4.91622L16.9838 1.91626Z"
-                      fill="#565656"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_3998_738">
-                      <rect
-                        width="19.9999"
-                        height="19.9997"
-                        fill="white"
-                        transform="translate(0 0.000244141)"
-                      />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </span>
-              <span></span>
             </div>
             <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
               <span className="text-md">
@@ -833,7 +900,8 @@ function SectionTwo() {
                   </defs>
                 </svg>
               </span>
-              <span></span>
+              <span className='text-sm w-2/3'>Ability to Add / modify / delete Manufacturer name, and Logo, which can be displayed on the product page.</span>
+
             </div>
             <div className="flex flex-row  justify-content items-baseline gap-2 md:gap-4">
               <span className="text-md">
@@ -862,7 +930,8 @@ function SectionTwo() {
                   </defs>
                 </svg>
               </span>
-              <span></span>
+              <span className='text-sm w-2/3'>Order management features, to view the order information and change in status</span>
+
             </div>
             </div>
 
