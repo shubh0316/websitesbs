b32b29d2c768afb111b18399da221b40f85e7873
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-15 01:29:07 +0530

    marquee-done

--- app/MarqueeSection.tsx
+++ app/MarqueeSection.tsx
@@ -1,28 +1,42 @@
 import React from "react";
-
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
 
+const data = [
+  "Web development",
+  "Software Services",
+  "MLM Company Setup",
+  "FDA Approvals",
+  "Import & Export",
+  "Party Plan",
+  "Uni Level Plan",
+  "Binary Plan",
+  "ROI Plan",
+  "Matrix Plan",
+  "Digital Marketing"
+];
 
 const MarqueeSection = () => {
   return (
-    <div  className="sm:overflow-hidden">  
-    <div className="mt-4 items-center">  
-    <div className="bg-white p-3 md:p-10 mt-20 max-w-sm rounded-xl md:max-w-7xl mx-auto overflow-x-hidden">
-      <div className="marquee-container overflow-x-hidden whitespace-nowrap">
-        <Marquee speed={100} pauseOnHover={true} autoFill={true}>
-          <div className="flex p-4 gap-10 justify-between">
-            <div className="py-2">  
-          <svg xmlns="http://www.w3.org/2000/svg" height="10" width="10">
-  <circle cx="5" cy="5" r="5" fill="orange" />
-</svg>
-</div>
-           <h1>Web Development</h1>
+    <div className="sm:overflow-hidden">
+      <div className="mt-4 items-center">
+        <div className="bg-white p-3 md:p-10 mt-20 max-w-sm rounded-xl md:max-w-7xl mx-auto overflow-x-hidden">
+          <div className="marquee-container overflow-x-hidden whitespace-nowrap">
+            <Marquee speed={100} pauseOnHover={true} autoFill={true}>
+              {data.map((value, index) => (
+                <div key={index} className="flex items-center px-8 gap-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="10" width="10">
+                    <circle cx="5" cy="5" r="5" fill="orange" />
+                  </svg>
+                  <div>
+                    <h4>{value}</h4>
+                  </div>
+                </div>
+              ))}
+            </Marquee>
           </div>
-        </Marquee>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 };
