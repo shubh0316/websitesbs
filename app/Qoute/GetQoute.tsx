d02b7cf7fd56e32f606d1e856e5a88a8e713ba69
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-15 21:17:43 +0530

    quotation-done

--- app/Qoute/GetQoute.tsx
+++ app/Qoute/GetQoute.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 
-const GetQoute = () => {
+const GetQuote = () => {
   return (
-    <>
-      <div  className="my-10">
+    <div className="flex justify-center items-center h-screen">
+      <div className="my-10 mx-auto max-w-4xl">
         <div className="flex flex-col text-center gap-y-5 py-10">
-          <div className="text-2xl md:text-4xl font-bold">QUOTATION</div>   
+          <div className="text-2xl md:text-4xl font-bold">QUOTATION</div>
         </div>
-        <div className="grid md:grid-cols-2 gap-10  p-10 max-w-4xl">
-         
+        <div className="grid md:grid-cols-2 gap-10 p-5 ">
           <div className="col-span-1">
             <div className="flex flex-col space-y-8">
-              
               <div className="text-start">
                 <h3 className="text-md mb-2 mt-2 font-bold">Name</h3>
                 <div className="rounded-full p-4 shadow-md">
                   <input
                     type="text"
-                    className="w-full h-full outline-none"
+                    className="w-80 h-full outline-none"
                     placeholder="Name"
                   />
                 </div>
@@ -46,7 +44,6 @@ const GetQoute = () => {
           </div>
           <div className="col-span-1">
             <div className="flex flex-col space-y-8">
-              
               <div className="text-start">
                 <h3 className="text-md mb-2 mt-2 font-bold">Service</h3>
                 <div className="rounded-full p-4 shadow-md">
@@ -81,14 +78,13 @@ const GetQoute = () => {
           </div>
         </div>
         <div className="mt-4 flex justify-center">
-        <div className="w-40 border border-blue-500 bg-white text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
-               Submit
-          </div>  
+          <div className="w-40 border border-blue-500 bg-white text-center text-blue-500 py-2 transition duration-500 ease-in-out hover:scale-110 rounded-full hover:bg-blue-500 hover:text-white">
+            Submit
           </div>
-
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default GetQoute;
+export default GetQuote;
