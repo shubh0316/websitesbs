e9847b95688ad62270000006df0f81f281e44fe0
shubhanker saxena <ankursaxena347@gmail.com>
2024-01-15 22:20:47 +0530

    hero-section-fixed

--- app/HeroSection.tsx
+++ app/HeroSection.tsx
@@ -16,8 +16,7 @@ const HeroSection = () => {
         <div className="flex flex-row space-y-4  ">
        
              <div className="md:py-40 p-4 md:p-10 max-w-4xl">
-                   <h3 className="p-2 text-orange-600 font-bold text-lg pb-4  ">DIGITAL AGENCY-</h3>
-                   <h3 className="p-2 text-4xl leading-tight font-bold md:w-full pb-4">An agency for all your business needs.</h3>
+                   <h3 className="p-2 text-4xl leading-tight font-bold md:w-full pb-4">Consultant for all your business needs.</h3>
                    <h3 className="p-2 text-md text-slate-700">MLM Consultant Asia is a leading India based Software Company focused on delivering the best and most cost-effective solutions to our clients. Mini InfoTech provide variety of software services, Software Development Services, Web Development Services, Web Designing Services, MLM Software and website, Hosting services and much more. We have served our clients with Comprehensive Web Solutions - right From Strategic Planning and Online Marketing Decisions to the actual Designing, Development and Implementation with testing of the Projects, and our Support Team is always there for the Maintenance and Enhancement of the Project.</h3>
                    <div className="py-4">  
                    <Link href="/Qoute"  className="bg-blue-600 text-white rounded-full px-5 py-2.5">Get a Quote</Link>
